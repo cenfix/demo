874e246e5d6e3bfeca30d9b0f51bcc5e54f7c93b
cenfix <89404695+cenfix@users.noreply.github.com>
2021-09-08 13:28:15 +0530

    Initial commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var z = document.getElementById("myTopnav");
+    if (z.className === "topnav") {
+      z.className += " responsive";
+    } else {
+      z.className = "topnav";
+    }
+  }
+
+
+function changeColor(y) 
+
+{
+    var x=document.getElementById("color");
+    x.style.backgroundColor= y
+}
+document.getElementById("demo").innerHTML = xhttp.responseText;
